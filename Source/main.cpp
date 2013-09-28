5023e53e4b7046e48676e5bca4b8a4305d3568cc
Grundkurs <commanderkeen83@googlemail.com>
2013-09-28 13:34:13 +0200

    XML.h and XML.cpp added

--- Source/main.cpp
+++ Source/main.cpp
@@ -8,11 +8,16 @@ int main()
 	{
 
     if(!XML::Init_XML_File("config.xml"))
-	{
+        {
 		std::cout << "could not open Config-File. Terminate now."; 
 		return 1; 
-	}
+        }
     int height = XML::LoadContentInt("WINDOW", "HEIGHT");
     float ratio = ((float)XML::LoadContentInt("WINDOW", "RATIO")) / 9;
+    Game game(height * ratio, height);
 
+    if(game.Initialize())
+        {
+        return game.Run();
+        }
     }
